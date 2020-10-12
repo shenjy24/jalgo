4db0f915b35e6f55e7029564c99d3314ed791e34
N14034 <shenjiayun@corp.netease.com>
2020-10-12 17:00:32 +0800

    基数排序

--- src/main/java/com/jonas/sort/RadixSort.java
+++ src/main/java/com/jonas/sort/RadixSort.java
@@ -1,5 +1,7 @@
 package com.jonas.sort;
 
+import java.util.Arrays;
+
 /**
  * 基数排序
  *
@@ -9,4 +11,33 @@ package com.jonas.sort;
  */
 public class RadixSort {
 
+    public static void main(String[] args) {
+        RadixSort app = new RadixSort();
+        int[] nums = new int[]{73, 22, 93, 1043, 55, 214, 214, 28, 165, 39, 81};
+        app.sort(nums, 4);
+        Arrays.stream(nums).asLongStream().forEach(System.out::println);
+    }
+
+    public void sort(int[] nums, int d) {
+        int[][] array = new int[10][nums.length];
+        int[] order = new int[10];
+        int n = 1;
+        while (n <= d) {
+            for (int num : nums) {
+                int index = (num / (int) Math.pow(10, n - 1)) % 10;
+                array[index][order[index]] = num;
+                order[index]++;
+            }
+
+            int k = 0;
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < order[i]; j++) {
+                    nums[k] = array[i][j];
+                    k++;
+                }
+                order[i] = 0;
+            }
+            n++;
+        }
+    }
 }
